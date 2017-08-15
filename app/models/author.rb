47fd73782ff62bb2294f9ad50d0257ef5744848f
github username <github email address>
2017-08-15 14:14:47 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+
+	validates :name, presence: true # all authors have a name
+	validates :name, uniqueness: true # no two authors have the same name
+	validates :phone_number, length: { is: 10 } # author phone numbers are exactly ten digits
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true # all posts have a title
+	validates :content, length: { minimum: 250 } # post content is at least 250 characters long
+	validates :summary, length: { maximum: 250 } # post summary is maximum 250 characters long
+	validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] } # post category is either fiction or non-fiction, requires an inclusion validator
+
+	validate :click_bait
+
+	def click_bait
+		click_bait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+		if self.title
+			unless click_bait.any? do |bait|
+				self.title.include?(bait)
+				end
+				self.errors.add(:title, "this isn't click bait")
+			end
+		end
+	end
+
+
 end
